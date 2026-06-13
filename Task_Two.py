27278061a65b7306b809530f16932eec7d774925
Juliet Udeh <Udehjuliet48@gmail.com>
2026-06-13 09:32:30 +0100

    Add expense calculation

---
+++ Task_Two.py
@@ -0,0 +1,7 @@
+total = 0
+
+total += float(input("Enter first expense: "))
+total += float(input("Enter second expense: "))
+total += float(input("Enter third expense: "))
+
+print("Total Spent:", total)
